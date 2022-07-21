cb77787ffabd89406cc8f13f629b3d01450a4a2b
Jason <jasonsu9323@gmail.com>
2022-07-21 10:13:12 -0400

    finished sample

--- src/HomePage/PopularList.js
+++ src/HomePage/PopularList.js
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/Col';
 
 export default function PopularList(){
     const rose = '/assets/rose.jpg';
+    const f1 = '/assets/f1.png';
+    const f2 = '/assets/f2.png';
     return(
         <div>
             <Container fluid>
@@ -13,15 +15,15 @@ export default function PopularList(){
                     <Col className ="m-auto d-block">
                         <Row>
                             <Col className = "d-flex justify-content-center">
-                                <FlowerCard img = {process.env.PUBLIC_URL + `${rose}`} name = {"Rosa"} dsc = {"This is a sample flower"} price = "$10"/>
+                                <FlowerCard img = {process.env.PUBLIC_URL + `${rose}`} name = {"Rosa"} dsc = {"Elegant and beautiful..."} price = "$12"/>
                             </Col>
                             <Col xs={1}/>
                             <Col className = "d-flex justify-content-center">
-                                <FlowerCard img = {process.env.PUBLIC_URL + `${rose}`} name = {"Rosa"} dsc = {"This is a sample flower"} price = "$10"/>
+                                <FlowerCard img = {process.env.PUBLIC_URL + `${f1}`} name = {"Elegant Pink"} dsc = {"A pink bomb"} price = "$15"/>
                             </Col>
                             <Col xs={1}/>
                             <Col className = "d-flex justify-content-center">
-                                <FlowerCard img = {process.env.PUBLIC_URL + `${rose}`} name = {"Rosa"} dsc = {"This is a sample flower"} price = "$10"/>
+                                <FlowerCard img = {process.env.PUBLIC_URL + `${f2}`} name = {"Koffee"} dsc = {"Accompanies you while you drink coffee"} price = "$13"/>
                             </Col>
                         </Row>
                     </Col>
--- src/SearchPage/Results.js
+++ src/SearchPage/Results.js
@@ -8,22 +8,28 @@ import {CaretLeftFill} from 'react-bootstrap-icons';
 
 export default function Results(){
     const rose = "/assets/rose.jpg";
+    const f1 = "/assets/f1.png";
+    const f2 = "/assets/f2.png";
+    const f3 = "/assets/f3.png";
+    const f4 = "/assets/f4.png";
+    const f5 = "/assets/f5.png";
+    
     return (
         <div className = "border mb-5">
             <Container>
                 <Row>
-                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Sample Flower" price = "$10"/></Col>
+                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Elegant and beautiful..." price = "$12"/></Col>
                     <Col xs={1}/>
-                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Sample Flower" price = "$10"/></Col>
+                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${f1}`} name = "Elegant Pink" dsc = "A pink bomb." price = "$15"/></Col>
                     <Col xs={1}/>
-                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Sample Flower" price = "$10"/></Col>
+                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${f2}`} name = "Koffee" dsc = "Accompanies you while you drink coffee :)" price = "$13"/></Col>
                 </Row>
                 <Row>
-                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Sample Flower" price = "$10"/></Col>
+                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${f3}`} name = "Peacock" dsc = "Accessorize your home!" price = "$14"/></Col>
                     <Col xs={1}/>
-                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Sample Flower" price = "$10"/></Col>
+                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${f4}`} name = "Chocolate with Roses" dsc = "Present the flowers... and then the chocolates beneath." price = "$50"/></Col>
                     <Col xs={1}/>
-                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${rose}`} name = "Rosa" dsc = "Sample Flower" price = "$10"/></Col>
+                    <Col xs = {3}><ResFlowerCard image = {process.env.PUBLIC_URL + `${f5}`} name = "Pink Mini" dsc = "A smaller and pink-er version of 'Chocolate with Roses'" price = "$25"/></Col>
                 </Row>
                 <Row>
                     <Col></Col>
