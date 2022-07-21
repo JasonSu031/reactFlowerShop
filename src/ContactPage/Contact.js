9112a4ec8f11470e9cd95b15b6a4c0f5604ca23c
Jason <jasonsu9323@gmail.com>
2022-07-21 00:11:21 -0400

    finished Design

--- src/ContactPage/Contact.js
+++ src/ContactPage/Contact.js
@@ -1,10 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-
+import {TelephoneFill} from 'react-bootstrap-icons';
+import {EnvelopeFill} from 'react-bootstrap-icons';
+import {GeoAltFill} from 'react-bootstrap-icons';
+import {ClockFill} from 'react-bootstrap-icons';
 
 export default function Contact(){
     return(
         <div className = "my-5">
-            Contact
+            <Container>
+                <Row>
+                    <Col xs = {3}>
+                    </Col>
+                    <Col xs = {3} className = "border">
+                        <img className = "img-fluid img-responsive" src = "/assets/admin.png"></img>
+                    </Col>
+                    <Col  xs = {4} className = "border centerText bg-light">
+                        <br/>
+                        <h5>Contact the Store Owner!</h5><br/>
+                        <TelephoneFill/> : 123-456-7890 <br/>
+                        <EnvelopeFill/>  : SimplyFlowers@hotmail.com <br/>
+                        <GeoAltFill/>  : 7 Simply Flowers st. Ottawa <br/>
+                        <br/>
+                        <ClockFill/> : Opening Times:<br/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Weekdays: 9:00-9:00 <br/>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Weekends: 9:00-9:00 <br/>
+                    </Col>
+                    <Col  xs = {2}>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
--- src/DesignPage/Design.js
+++ src/DesignPage/Design.js
@@ -1,12 +1,43 @@
-
-
-
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import {Link} from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function Design(){
     return(
         <div className = "my-5">
-            Design
+            <Container>
+                <Row>
+                    <Col xs = {2}></Col>
+                    <Col xs= {8} className = "border bg-light px-4 py-3">
+                        <Form.Label>Upload an image of your flower design:</Form.Label>
+                        <Form.Control type = "file"></Form.Control>
+                        <br/>
+                        <Form.Label>Follow the design:</Form.Label>
+                        <Form.Select>
+                            <option>Exactly</option>
+                            <option>Similar Design</option>
+                            <option>Similar Mood</option>
+                            <option>Take Inspiration</option>
+                        </Form.Select>
+                        <br/>
+                        <Form.Label>What feelings are you trying to portray?</Form.Label>
+                        <Form.Control as = "textarea">
+                        </Form.Control>
+                        <br/>
+                        <Form.Label>Elements that must be included in the design:</Form.Label>
+                        <Form.Control as = "textarea">
+                        </Form.Control>
+                        <br/>
+                        <Link to = "/success" className = "text-decoration-none" state = {{status:"Submission"}}>
+                            <Button variant = "success" className = "py-1 my-4 d-block mx-auto">Submit</Button>
+                        </Link>
+                    </Col>
+                    <Col xs = {2}></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
